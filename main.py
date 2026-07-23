54bfa21e7dcf2d1b1b77992566c07f86a49f6ba0
BindhuSri2321 <gandikotabindhusri@gmail.com>
2026-07-23 12:29:00 +0530

    day1

---
+++ main.py
@@ -0,0 +1,14 @@
+dict={
+"name":"Bindhu Sri",
+"gender":"female",
+"age":20,
+"courses":["python","java"]
+}
+dict["name"]="Ammu"
+dict.update({"name":"Ammu"})
+print(dict)
+print(dict.keys)
+print(dict.items)
+print(dict.values)
+print(dict["age"])
+print(dict.get("name"))
